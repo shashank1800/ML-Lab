30bc2c8d887ab3cb48c099a32193ae1ccf2b12d9
Shashank Bhat <shashankbhat1800@gmail.com>
2019-10-28 22:08:54 +0530

    Update lab1.py

--- 1/lab1.py
+++ 1/lab1.py
@@ -1,16 +1,20 @@
 import csv
+
 with open('trainingexamples.csv') as csvFile:
     data = [line[:-1] for line in csv.reader(csvFile) if line[-1] == "Y"]
-    
-for d in data:
-    print(d)
-print("")
 
-S = ['@']*len(data[0])   # Initializing.
-print("output in each steps are:\n{}".format(S))
-for example in data:
+S = data[0]
+print("POSITIVE EXAMPLES ARE: ",data)
+print("Output in each steps are: \n")
+print(['@']*len(S))
+print(S)
+
+for row in data:
+    if row==S:
+        continue
     i = 0
-    for feature in example:
-        S[i] = feature if S[i] == '@' or S[i] == feature else '?'
+    for val in row:
+        if S[i]!=val:
+        	S[i] = '?'
         i += 1
     print(S)
